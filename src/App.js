379cfaccb8f2e809e35b1571b9b6363bd346ad34
Akhilesh-Yadav6283 <akhiyadav74085@gmail.com>
2024-07-28 09:26:50 +0530

    second time

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './components/Login';
 import Profile from './components/Profile';
-
+ 
 function App() {
   return (
     <Router>
